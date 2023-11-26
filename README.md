08c4cbcbe8001e0eaf0b87e7fa22795d18d2af05
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2023-11-26 21:32:11 +0000

    chore: initialise repository and cargo project

---
+++ README.md
@@ -0,0 +1,35 @@
+<div align=center>
+
+# ❄️ nix-template-rust-nightly 🦀
+
+[![NixOS](https://img.shields.io/badge/Made_for-Rust-orange.svg?logo=rust&style=for-the-badge)](https://www.rust-lang.org/) [![NixOS](https://img.shields.io/badge/Flakes-Nix-informational.svg?logo=nixos&style=for-the-badge)](https://nixos.org) ![License](https://img.shields.io/github/license/mordragt/nix-templates?style=for-the-badge)
+
+Minimal **Rust** development template for **Nix**
+
+</div>
+
+## About
+
+This is a minimal template for Rust development on the nightly channel. 
+
+## Initialization
+
+See the parent README for further instructions, but you can initialize this template
+with the following command in your current directory.
+
+```bash
+nix flake init -t github:MordragT/nix-templates#rust-nightly
+```
+
+## Usage
+
+- `nix develop`: opens up a `bash` shell with the bare minimum Rust toolset (`cargo` & `rustc`) by default
+- `nix build` : builds the Rust project. Outputs the binary to `./result/bin/<name>`
+- `nix run`: runs the Rust program.
+
+
+## Reference
+
+1. [wiki/Flakes](https://nixos.wiki/wiki/Flakes)
+2. [Fenix](https://github.com/nix-community/fenix) - used for managing Rust toolchains (read the `makeRustPlatform` example)
+3. [rust-section of language frameworks](https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features) - optional (use it for extending the template)
---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Rust development template using fenix";
+
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    fenix,
+    ...
+  }:
+    utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [fenix.overlays.default];
+        };
+        toolchain = pkgs.fenix.complete;
+      in rec
+      {
+        # Executed by `nix build`
+        packages.default =
+          (pkgs.makeRustPlatform {
+            # Use nightly rustc and cargo provided by fenix for building
+            inherit (toolchain) cargo rustc;
+          })
+          .buildRustPackage {
+            pname = "template";
+            version = "0.1.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+
+            # For other makeRustPlatform features see:
+            # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
+          };
+
+        # Executed by `nix run`
+        apps.default = utils.lib.mkApp {drv = packages.default;};
+
+        # Used by `nix develop`
+        devShells.default = pkgs.mkShell {
+          # Use nightly cargo & rustc provided by fenix. Add for packages for the dev shell here
+          buildInputs = with pkgs; [
+            (with toolchain; [
+              cargo rustc rust-src clippy rustfmt # rust components
+            ])
+            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
+            libxkbcommon wayland # To use the wayland feature
+
+            mold
+            clang
+            pkg-config
+          ];
+
+          # Specify the rust-src path (many editors rely on this)
+          RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+}
