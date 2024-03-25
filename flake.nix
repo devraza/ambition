163e37f159b51f8c6f1f46339d8e6e9e35f6f09e
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-03-25 22:46:36 +0000

    flake!: cleanup and remove Rust from devShell
    Users will now have to use a Rust toolchain provided outside of this repository. However, this project's specific depdencies, such as mold and clang, are provided in the `nix develop` environment.

--- flake.nix
+++ flake.nix
@@ -2,10 +2,6 @@
   description = "Rust development environment for Ambition using fenix";
 
   inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
     utils.url = "github:numtide/flake-utils";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixgl.url = "github:guibou/nixGL";
@@ -15,7 +11,6 @@
     self,
     nixpkgs-unstable,
     utils,
-    fenix,
     nixgl,
     ...
   }:
@@ -24,26 +19,17 @@
       system: let
         pkgs = import nixpkgs-unstable {
           inherit system;
-          overlays = [fenix.overlays.default nixgl.overlay];
+          overlays = [nixgl.overlay];
         };
-        toolchain = pkgs.fenix.complete;
       in rec
       {
         # Executed by `nix build`
-        packages.default =
-          (pkgs.makeRustPlatform {
-            # Use nightly rustc and cargo provided by fenix for building
-            inherit (toolchain) cargo rustc;
-          })
-          .buildRustPackage {
-            pname = "ambition";
-            version = "0.4.0";
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-
-            # For other makeRustPlatform features see:
-            # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
-          };
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "ambition";
+          version = "0.4.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
 
         # Executed by `nix run`
         apps.default = utils.lib.mkApp {drv = packages.default;};
@@ -56,25 +42,16 @@
             pkgs.alsa-lib
             pkgs.libxkbcommon
           ]}"'';
-          # Use nightly cargo & rustc provided by fenix. Add for packages for the dev shell here
           buildInputs = with pkgs; [
-            (with toolchain; [
-              cargo rustc rust-src clippy rustfmt # rust components
-            ])
             xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr # To use the x11 feature
             libxkbcommon # To use the wayland feature
-
             udev alsa-lib vulkan-loader
             pkgs.nixgl.nixVulkanIntel
-
             mold
             clang
             pkg-config
-            
             tokei
           ];
-          # Specify the rust-src path (many editors rely on this)
-          RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
         };
       }
     );
